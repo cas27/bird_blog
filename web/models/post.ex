9abf64a3db7bb7d15673c527fc5a58824ca286f1
Cory Schmitt <cory@schmitty.me>
2015-07-16 13:19:57 -0700

    Blank slug callback on insert

--- web/models/post.ex
+++ web/models/post.ex
@@ -1,6 +1,8 @@
 defmodule BirdBlog.Post do
   use BirdBlog.Web, :model
 
+  before_insert :generate_slug
+
   schema "posts" do
     field :title, :string
     field :slug, :string
@@ -9,8 +11,8 @@ defmodule BirdBlog.Post do
     timestamps
   end
 
-  @required_fields ~w(title body slug)
-  @optional_fields ~w()
+  @required_fields ~w(title body)
+  @optional_fields ~w(slug)
 
   @doc """
   Creates a changeset based on the `model` and `params`.
@@ -21,6 +23,21 @@ defmodule BirdBlog.Post do
   def changeset(model, params \\ :empty) do
     model
     |> cast(params, @required_fields, @optional_fields)
+  end
+
+  def title_to_slug({_, title}) do
+    title
+    |> String.downcase
+    |> String.replace(" ", "-")
+  end
+
+  defp generate_slug(changeset) do
+    case fetch_field(changeset, :slug) do
+      {:changes, nil} ->
+        changeset = put_change(changeset, :slug, title_to_slug(fetch_field(changeset, :title)))
+      _ ->
+        changeset
+     end
   end
 
 end

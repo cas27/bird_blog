97f3832d4a86cf41c9f74be9110e8dfb3cb18b17
Cory Schmitt <cory@schmitty.me>
2015-07-17 22:58:46 -0700

    Test post model validations and slug generation

--- test/models/post_test.exs
+++ test/models/post_test.exs
@@ -1,9 +1,11 @@
 defmodule BirdBlog.PostTest do
   use BirdBlog.ModelCase
+  import Ecto.Changeset, only: [fetch_field: 2]
 
   alias BirdBlog.Post
+  alias BirdBlog.Repo
 
-  @valid_attrs %{body: "some content", title: "some content"}
+  @valid_attrs %{body: "some content", title: "some content", slug: "what-up"}
   @invalid_attrs %{}
 
   test "changeset with valid attributes" do
@@ -14,5 +16,18 @@ defmodule BirdBlog.PostTest do
   test "changeset with invalid attributes" do
     changeset = Post.changeset(%Post{}, @invalid_attrs)
     refute changeset.valid?
+  end
+
+  test "two posts with the same slug" do
+    changeset = Post.changeset(%Post{}, @valid_attrs)
+    Repo.insert!(changeset)
+
+    changeset2 = Post.changeset(%Post{}, @valid_attrs)
+    refute changeset2.valid?
+  end
+
+  test "changeset with valid attributes" do
+    changeset = Post.changeset(%Post{}, Map.delete(@valid_attrs, :slug))
+    assert fetch_field(changeset, :slug) == {:changes, "some-content"}
   end
 end
